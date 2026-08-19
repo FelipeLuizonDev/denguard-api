0b96554de45c4f9de550bb119a2dc146a66d206c
FelipeLuizonDev <luizonfelipe@gmail.com>
2026-08-19 15:28:49 -0300

    chore(backend): set up PostgreSQL and backend environment

---
+++ src/main/java/com/denguard/DenguardApiApplication.java
@@ -0,0 +1,13 @@
+package com.denguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DenguardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DenguardApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/denguard/DenguardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DenguardApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
